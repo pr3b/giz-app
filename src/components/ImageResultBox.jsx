455df370a127147390d71958ce8a34c3b97985b7
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-04-09 09:20:26 +0700

    add title to img\ result

--- src/components/ImageResultBox.jsx
+++ src/components/ImageResultBox.jsx
@@ -1,16 +1,19 @@
 import { useImage } from '@/libs/userImageSrc';
 import Image from 'next/image';
-import React from 'react'
+import React, { Fragment } from 'react'
 
 function ImageResultBox() {
   const imageSrc = useImage();
 
   return (
-    <div className='flex-col p-0'>
-      {imageSrc && (
-        <Image  width={400} height={400} src={imageSrc} alt="Generated Mask"/>
-      )}
-    </div>
+    <Fragment>
+      <h1 className='font-bold text-2xl'>Potential Result</h1>
+      <div className='flex-col p-0'>
+        {imageSrc && (
+          <Image  width={400} height={400} src={imageSrc} alt="Generated Mask"/>
+        )}
+      </div>
+    </Fragment>
   )
 }
 
